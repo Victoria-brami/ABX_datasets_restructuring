2678ccd081da56e3eb802c8ed170b7ea2f3c5695
Votre Nom <Vous@exemple.com>
2021-03-12 17:26:04 +0100

    update

--- stimuli_wav/zero_speech_create_extracted_stimuli.py
+++ stimuli_wav/zero_speech_create_extracted_stimuli.py
@@ -1,12 +1,10 @@
 # import numpy as np
-from scipy.io import wavfile
 import pandas as pd
-import os.path as osp
-import os
+from scipy.io import wavfile
 
-def slice_wav(wav_file, index, begin, end, newfilename):
 
-	"""
+def slice_wav(wav_file, begin, end, newfilename):
+    """
 	This function slice an array into two values given as begin and end
 	Args:
 		WavArray: an array obtained by a wav file
@@ -17,38 +15,34 @@ def slice_wav(wav_file, index, begin, end, newfilename):
 		A new wav file which contains extracted part
 
 	"""
-	freq, data=wavfile.read(wav_file)
-	new_sliced_array=data[(int(begin*freq)):(int(end*freq))]
-	wavfile.write(newfilename,freq,new_sliced_array)
-
+    freq, data = wavfile.read(wav_file)
+    new_sliced_array = data[(int(begin * freq)):(int(end * freq))]
+    wavfile.write(newfilename, freq, new_sliced_array)
 
 
 if __name__ == '__main__':
 
-    WAV_META_INFO = '/home/coml/Documents/Victoria/data/pilote/pilote_data_july_2018_stimuli.csv'
-    WAV_SOURCE_FOLDER = '/home/coml/Documents/Victoria/data/pilote/sound_data/wavs_source/timit/'
-    WAV_EXTRACTED_FOLDER = '/home/coml/Documents/Victoria/data/pilote/sound_data/wavs_extracted'
-
+    WAV_META_INFO = '/home/coml/Documents/Victoria/data/zero_speech/zero_speech_dataset_stimuli.csv'
+    WAV_SOURCE_FOLDER = '/home/coml/Documents/Victoria/data/zero_speech/sound_data/wavs_source/'
+    WAV_EXTRACTED_FOLDER = '/home/coml/Documents/Victoria/data/zero_speech/sound_data/wavs_extracted/'
 
     # store stimuli info
     stimuli_info = pd.read_csv(WAV_META_INFO)
 
     # split the wavs
-    for i in range(len(stimuli_info['#file'])):
+    for i in range(39, len(stimuli_info['#file'])):
 
-        index = stimuli_info['#file']
-        onset = stimuli_info['#file']
-        offset = stimuli_info['#file']
-        wav_file = WAV_SOURCE_FOLDER + stimuli_info['#file'] + 'WAV.wav'
+        if stimuli_info['#file'][i] != '1s_french/8682.wav':
+            index = stimuli_info['index'][i]
+            onset = stimuli_info['onset'][i]
+            offset = stimuli_info['offset'][i]
 
+            print(stimuli_info['#file'][i])
 
-        stimuli_folder = stimuli_info['#file'].split('/')
-        stimuli_folder.pop()
-        stimuli_folder = '/'.join(stimuli_folder)
-        if not osp.join(WAV_EXTRACTED_FOLDER, stimuli_folder).exists():
-            os.mkdir(osp.join(WAV_EXTRACTED_FOLDER, stimuli_folder))
+            wav_file = WAV_SOURCE_FOLDER + stimuli_info['#file'][i]
 
-        extracted_wav_file = osp.join(WAV_EXTRACTED_FOLDER, stimuli_folder)
+            extracted_wav_file = WAV_EXTRACTED_FOLDER + stimuli_info['#file'][i].split('.')[0] + '_sliced_' + str(index) + '.wav'
 
+            slice_wav(wav_file, onset, offset, extracted_wav_file)
 
-	print('Done')
+    print('Done')
